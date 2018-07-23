2d9b29378a7ee3166787717de9a1ad307141038e
W3bsafe-Team <1449485239@qq.com>
2018-07-23 14:52:58 +0800

    Update Source

--- README.md
+++ README.md
@@ -1,1 +1,78 @@
 # Wemos_esp8266_deauther
+由W3bsafe-Team Angel 二次开发<br />
+适配于Wemos ESP8266 Oled开发板<br />
+WIFI初始用户名：W3bsafe-Team 密码：deauther<br />
+直接使用Arduino IDE编译即可<br />
+<br />
+<br />
+<br />
+<br />
+
+<h2>ESP8266 Deauther</h2>
+<p>
+	<span class="bold">Copyright (c) 2018 Stefan Kremser</span><br />
+	<br />
+	This project is licensed under the MIT License. See the <a href="LICENSE">license file</a> for details.<br />
+	The source code is available on <a href="https://github.com/spacehuhn/esp8266_deauther" target="_blank">GitHub</a>.<br />
+	<br />
+	Use it only for testing or educational purposes against your own networks and devices!<br />
+	<br />
+	<span class="bold">Please don't refer to this project as "jammer", that totally undermines the real purpose of this project!</span>
+</p>
+
+<h2>Credits</h2>
+<p>
+	A huge thanks to:<br>
+	<ul>
+		<li><a href="http://github.com/deantonious" target="_blank">@deantonious</a></li>
+		<li><a href="http://github.com/jLynx" target="_blank">@jLynx</a></li>
+		<li><a href="http://github.com/lspoplove" target="_blank">@lspoplove</a></li>
+		<li><a href="http://github.com/schinfo" target="_blank">@schinfo</a></li>
+		<li><a href="http://github.com/tobozo" target="_blank">@tobozo</a></li>
+		<li><a href="http://github.com/xdavidhu" target="_blank">@xdavidhu</a></li>
+		<li><a href="http://github.com/PwnKitteh" target="_blank">@PwnKitteh</a></li>
+	</ul>
+	for helping out with various things regarding this project and keeping it alive!<br>
+	<br>
+	I also want to thank Espressif and their community for this awesome chip and all the software and hardware projects around it and the countless tutorials you can find online!<br>
+	<br>
+	Shoutout to everyone working on the libraries used for this project:<br>
+	<ul>
+		<li><a href="https://github.com/ThingPulse/esp8266-oled-ssd1306" target="_blank">esp8266-oled-ssd1306</a></li>
+		<li><a href="https://github.com/bblanchon/ArduinoJson" target="_blank">ArduinoJson</a></li>
+		<li><a href="https://github.com/ivanseidel/LinkedList" target="_blank">LinkedList</a></li>
+	</ul>
+	<br>
+	Also thanks to everyone that supports this project by <a href="http://spacehuhn.de/donate" target="_blank">donating]</a>, beeing my <a href="http://patreon.com/spacehuhn" target="_blank">patron</a> or buying one of the <a href="https://www.tindie.com/stores/lspoplove" target="_blank">official Deauther boards</a> from DSTIKE.<br>
+</p>
+
+<h2>Contact</h2>
+<p>
+	Web: <a target="_blank" href="http://spacehuhn.com/">spacehuhn.com</a><br>
+	GitHub: <a target="_blank" href="http://github.com/spacehuhn/">github.com/spacehuhn</a><br />
+	Twitter: <a target="_blank" href="http://twitter.com/spacehuhn/">@spacehuhn</a><br />
+	E-mail: mail@spacehuhn.com<br />
+	<br>
+	If you would like to support me with my projects, please consider becoming a patron on <a target="_blank" href="https://patreon.com/spacehuhn">patreon.com/spacehuhn</a>.<br />
+	<br>
+	Or buy the official hardware for this project from DSTIKE on <a target="_blank" href="https://www.tindie.com/stores/lspoplove/">tindie.com/stores/lspoplove</a>.
+</p>
+					
+<h2>二次开发</h2>
+<p>
+	团队: <a target="_blank" href="http://www.w3bsafe.cn">W3bsafe-Team</a><br>
+	GitHub: <a target="_blank" href="https://github.com/W3bSafe-Team">github.com/W3bSafe-Team</a><br />
+	E-mail: Angel@w3bsafe.cn<br />
+	<br>
+	我们尊重MIT开源许可协议，感谢源作者@spacehuhn<br />
+	</a>.
+</p>
+
+<h2>硬件开发</h2>
+<p>
+	公司: 深圳市育松电子<br>
+	淘宝: <a target="_blank" href="https://shop33817767.taobao.com/">深圳市育松电子 元器件 模块 工具</a><br />
+	<br>
+	我们尊重MIT开源许可协议，感谢源作者@spacehuhn<br />
+	</a>.
+</p>
---
+++ esp8266_deauther.ino
@@ -0,0 +1,166 @@
+ /*
+   ===========================================
+      Copyright (c) 2018 Stefan Kremser
+             github.com/spacehuhn
+   ===========================================
+ */
+extern "C" {
+  #include "user_interface.h"
+}
+#include <EEPROM.h>
+#include "oui.h"
+#include "language.h"
+#include "functions.h"
+#include "Settings.h"
+#include "Names.h"
+#include "SSIDs.h"
+#include "Scan.h"
+#include "Attack.h"
+#include "SerialInterface.h"
+#include "DisplayUI.h"
+#include "A_config.h"
+#include "webfiles.h"
+
+#include "LEDController.h"
+
+// Run-Time Variables //
+LEDController* led;
+Settings settings;
+Names    names;
+SSIDs    ssids;
+Accesspoints accesspoints;
+Stations     stations;
+Scan   scan;
+Attack attack;
+SerialInterface serialInterface;
+DisplayUI displayUI;
+
+#include "wifi.h"
+
+uint32_t autosaveTime = 0;
+uint32_t currentTime  = 0;
+
+bool booted = false;
+
+void setup() {
+    // for random generator
+    randomSeed(os_random());
+
+    // start serial
+    Serial.begin(115200);
+    Serial.println();
+
+    // start SPIFFS
+    prnt(SETUP_MOUNT_SPIFFS);
+    prntln(SPIFFS.begin() ? SETUP_OK : SETUP_ERROR);
+
+    // Start EEPROM
+    EEPROM.begin(4096);
+
+    // auto repair when in boot-loop
+    uint8_t bootCounter = EEPROM.read(0);
+
+    if (bootCounter >= 3) {
+        prnt(SETUP_FORMAT_SPIFFS);
+        SPIFFS.format();
+        prntln(SETUP_OK);
+    } else {
+        EEPROM.write(0, bootCounter + 1); // add 1 to the boot counter
+        EEPROM.commit();
+    }
+
+    // get time
+    currentTime = millis();
+
+    // load settings
+    settings.load();
+
+    // set mac for access point
+    wifi_set_macaddr(SOFTAP_IF, settings.getMacAP());
+
+    // start WiFi
+    WiFi.mode(WIFI_OFF);
+    wifi_set_opmode(STATION_MODE);
+    wifi_set_promiscuous_rx_cb([](uint8_t* buf, uint16_t len) {
+        scan.sniffer(buf, len);
+    });
+
+    // set mac for station
+    wifi_set_macaddr(STATION_IF, settings.getMacSt());
+
+    // start display
+    if (settings.getDisplayInterface()) {
+        displayUI.setup();
+        displayUI.mode = SCREEN_MODE_INTRO;
+    }
+
+    // copy web files to SPIFFS
+    copyWebFiles(false);
+
+    // load everything else
+    names.load();
+    ssids.load();
+    serialInterface.load();
+
+    // create scan.json
+    scan.setup();
+
+    // setup LED
+    led = new LEDController();
+    led->setup();
+
+    // set channel
+    setWifiChannel(settings.getChannel());
+
+    // load Wifi settings: SSID, password,...
+    #ifdef DEFAULT_SSID
+      if(settings.getSSID() == "W3bsafe-Team") settings.setSSID(DEFAULT_SSID);
+    #endif
+    loadWifiConfigDefaults();
+
+    // dis/enable serial command interface
+    if (settings.getSerialInterface()) {
+        serialInterface.enable();
+    } else {
+        prntln(SETUP_SERIAL_WARNING);
+        Serial.flush();
+        Serial.end();
+    }
+
+    // start access point/web interface
+    if (settings.getWebInterface()) startAP();
+
+    // STARTED
+    prntln(SETUP_STARTED);
+
+    // version
+    prntln(settings.getVersion());
+}
+
+void loop() {
+    currentTime = millis();
+
+    wifiUpdate();             // manage access point
+
+    attack.update();          // run attacks
+    displayUI.update();
+    serialInterface.update(); // read and run serial input
+    scan.update();            // run scan
+    ssids.update();           // run random mode, if enabled
+    led->update();            // update LED color
+
+    // auto-save
+    if (settings.getAutosave() && (currentTime - autosaveTime > settings.getAutosaveTime())) {
+        autosaveTime = currentTime;
+        names.save(false);
+        ssids.save(false);
+        settings.save(false);
+    }
+
+    if (!booted) {
+        // reset boot counter
+        EEPROM.write(0, 0);
+        EEPROM.commit();
+        booted = true;
+    }
+}
